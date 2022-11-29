69a1626385c32535448d1b614c291e78a8b310cf
franmer2 <franmer@microsoft.com>
2022-11-28 23:23:38 -0500

    Add PowerShell scripts for DUM

---
+++ PowerShell/DUM_AzureSQL.ps1
@@ -0,0 +1,9 @@
+﻿Connect-AzAccount -UseDeviceAuthentication -TenantId xxxx-xxxx-xxxx-xxxx-xxxx -SubscriptionId xxxx-xxxx-xxxx-xxxx
+
+$server = Get-AzSqlServer -ResourceGroupName "RESOURCEGROUPNAME" -ServerName "SERVERNAME"
+
+#Initiate the call to the REST API to set externalPolicyBasedAuthorization to true
+Invoke-AzRestMethod -Method PUT -Path "$($server.ResourceId)/externalPolicyBasedAuthorizations/MicrosoftPurview?api-version=2021-11-01-preview" -Payload '{"properties":{"externalPolicyBasedAuthorization":true}}'
+
+# Now, verify that the property "externalPolicyBasedAuthorization" has been set to true
+Invoke-AzRestMethod -Method GET -Path "$($server.ResourceId)/externalPolicyBasedAuthorizations/MicrosoftPurview?api-version=2021-11-01-preview"
---
+++ PowerShell/DUM_AzureStorage.ps1
@@ -0,0 +1,6 @@
+﻿# Install the Az module
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+# Login into the subscription
+Connect-AzAccount -Subscription <subsciptionID>
+# Register the feature
+Register-AzProviderFeature -FeatureName AllowPurviewPolicyEnforcement -ProviderNamespace Microsoft.Storage
