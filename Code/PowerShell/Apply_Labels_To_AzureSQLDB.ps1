b908c8d37fa5922d80220ea02079d2345b94fc3c
franmer2 <franmer@microsoft.com>
2023-02-12 17:08:29 -0500

    V1

---
+++ Code/PowerShell/Apply_Labels_To_AzureSQLDB.ps1
@@ -0,0 +1,83 @@
+﻿# Replace the values below with the relevant values for your environment
+$TenantID = ''
+$ClientID = ''
+$SecretID = ''
+$purviewAccountName=''
+$sqlDatabaseName=""
+
+###############################################################################
+# Get an access accessToken, and build REST Request Header.
+###############################################################################
+$cmdletParameters = @{
+  Method  = "POST"
+  URI     = "https://login.microsoftonline.com/$TenantID/oauth2/token"
+  Headers = @{
+    "Content-Type" = 'application/x-www-form-urlencoded'
+  }
+  Body    = "client_id=$ClientID&client_secret=$SecretID&resource=https://purview.azure.net&grant_type=client_credentials"
+}
+
+$invokeResult = Invoke-RestMethod @cmdletParameters;
+$accessToken = $invokeResult.access_token; 
+
+$restRequestHeader = @{
+  "authorization" = "Bearer $accessToken"
+};
+
+###############################################################################
+# Get database entity.
+###############################################################################
+$getDatabaseEntityEntryEndpoint = "https://" + $purviewAccountName + ".catalog.purview.azure.com/api/atlas/v2/entity/uniqueAttribute/type/azure_sql_db?attr:qualifiedName=" + $sqlDatabaseName;
+$cmdletParameters = @{
+  Method      = "Get"
+  Uri         = $getDatabaseEntityEntryEndpoint
+  Headers     = $restRequestHeader
+  ContentType = "application/json"        
+};
+
+$invokeResult =  Invoke-RestMethod @cmdletParameters;
+$referredEntities = $invokeResult.referredEntities;
+if ($null -eq $referredEntities) {
+  Write-Output "No referred entities found under database entity!";
+  exit;
+}
+
+###############################################################################
+# Iterate database referred entities, find classified columns, and build T-SQL.
+###############################################################################
+foreach ($referredEntity in $referredEntities.psobject.Properties.GetEnumerator()) {
+  $typeName = $referredEntity.Value.typeName;
+  if ($null -eq $typeName -or $typeName -ne 'azure_sql_column') {
+    continue;
+  }
+  
+  $classifications = $referredEntity.Value.classifications;
+  if ($null -eq $classifications) {
+    continue;
+  }
+  
+  foreach ($classification in $classifications.GetEnumerator())
+  {
+    if ($classification.typeName -notmatch 'Microsoft\.Label\.(?<labelId>.+)') { 
+        continue;
+    }
+
+      $labelId = $Matches.labelId -replace "_", "-";
+
+      $attributes = $referredEntity.Value.attributes;
+      if ($null -eq $attributes) {
+        continue;
+      }
+    
+      $qualifiedName = $attributes.qualifiedName;
+      if ($qualifiedName -notmatch '.+\.database\.windows\.net\/.+\/(?<schemaName>.+)\/(?<tableName>.+)#(?<columnName>.+)') {
+        continue;
+      }
+
+      $schemaName = $Matches.schemaName;
+      $tableName = $Matches.tableName;
+      $columnName = $Matches.columnName;
+      
+      Write-Output "ADD SENSITIVITY CLASSIFICATION TO ${schemaName}.${tableName}.${columnName} WITH (LABEL='Azure Purview Label', LABEL_ID='${labelId}');";
+  }
+}
